9ddf4aadec0c803d1f80e1a7f9f4e0c6244584cf
wjdals1217 <wjdals1217@gmail.com>
2023-11-15 17:51:30 +0900

    JWT

--- ch07/src/main.js
+++ ch07/src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
 import router from "./router/index.js";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-createApp(App)
-.use(router)
-.mount('#app')
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+createApp(App).use(router).use(pinia).mount("#app");
--- ch07/src/router/index.js
+++ ch07/src/router/index.js
@@ -1,12 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AxiosComponent from "../components/AxiosComponent.vue";
 import JwtComponent from "../components/JwtComponent.vue";
+import Login from "../components/user/Login.vue";
+import LoginSuccess from "../components/user/LoginSuccess.vue";
+
 // router 인스턴스 생성
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/axios", name: "Axios", component: AxiosComponent },
-    { path: "/jwt", name: "Jwt", component: JwtComponent },
+    {
+      path: "/jwt",
+      name: "Jwt",
+      component: JwtComponent,
+      children: [
+        { path: "", component: Login },
+        { path: "login", component: Login },
+        { path: "loginSuccess", component: LoginSuccess },
+      ],
+    },
   ],
 });
 
